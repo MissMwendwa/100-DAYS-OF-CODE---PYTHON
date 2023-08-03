9b14429bfe0c3a927c9bd90fac7b8956f5095941
MissMwendwa <wanjamwesh@gmail.com>
2023-08-03 21:26:19 +0300

    first commit

---
+++ prints.py
@@ -0,0 +1,18 @@
+#writing my first Hello World
+print("Hello  World!")
+
+# More sophisticated print statement
+print("I am a Girl")
+print("I am 30 years old!")
+
+#anothrer print statement
+print("I am working aT Elewa Tech Academy. I am a new Instructor. I love my job!")
+
+# Print Assignment
+print(" My name is Miss Mwendwa")
+print('I am currently not that old')
+print(" I hope I'll be able to compelete the course content for today!")
+
+# Extra Print statements
+print(" I am a Python engineer")
+print(" ")
---
+++ variables.py
@@ -0,0 +1,49 @@
+# Understanding variables in Python
+# I will be assigning different variables to different values
+
+#First Variable representation
+first_var = "Ashley"
+second_var = 38
+
+#print(first_var)
+#print(second_var)
+
+# Second Variable Represntation
+var_1 = "I love Travelling"
+var_2 = "I am stronger than I look"
+
+#print(var_1)
+#print(var_2)
+
+
+# Third Variable Representation
+school = "St. Angelas High school"
+Marks = " I had a B+"
+
+#print(school)
+#print(Marks)
+
+# Fourth Variable Representation
+Food = " Beef + Pasta"
+Desert = 'I like me a Banana Split'
+Salad = " Lemon Colestraw"
+
+print(Food)
+print(Salad)
+print(Desert)
+
+# Fifth Representation
+Class = "First class Honours"
+Course = " Software Engineering"
+School = " College of Technology"
+
+print(Class, School, Course)
+
+#Variables
+var = "Variables make coding easy to understand"
+var_2 = " Variables are super simple to understand"
+var_3 = " I lowkey love using variables when I'm coding"
+
+print(var)
+print(var_2)
+print(var_3)
