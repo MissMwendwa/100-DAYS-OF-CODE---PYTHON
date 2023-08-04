f673d55b51cd2a46a5ae5bd5136831bbac86ddcf
MissMwendwa <wanjamwesh@gmail.com>
2023-08-04 23:09:21 +0300

    Logical Operations

---
+++ Logical_ops.py
@@ -0,0 +1,26 @@
+#This is a logical operations file
+# I'm performing basic logical operations for demonstartion
+
+#Logical OR
+var_1 = 24
+var_2 = 30
+
+var = var_1 or var_2
+#print(var)
+
+#Logical AND
+var_3 = var_1 and var_2
+#print(var_3)
+
+#Logical NOT
+var_4 = not var_2
+#print(var_4)
+
+
+char_1 = True
+char_2 = False
+
+#Logical OR
+char = char_1 or char_2
+print(char)
+
