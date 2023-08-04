575397617b168d60e238468dc9dd26be3c26b951
MissMwendwa <wanjamwesh@gmail.com>
2023-08-04 21:28:19 +0300

    Python Operations

---
+++ arithimetic_ops.py
@@ -0,0 +1,55 @@
+# This is a demonstration ogf basic arithmetic operators in python
+#addition
+
+var_1 = 10
+var_2 = 4
+
+#code starts here
+#varibale additon
+var_3 = var_1 + var_2
+print(var_3)
+
+print(var_1 + var_2)
+
+#subtarction
+#method variables
+var_4 = var_1 - var_2
+print(var_4)
+
+#method printing
+print(var_1 - var_2)
+
+#multiplication
+#method varible
+var_5 = var_1 * var_2
+print(var_5)
+
+#method printing
+print(var_1 * var_2)
+
+#Division
+#method varible
+var_6 = var_1 / var_2
+print(var_6)
+
+#modulus
+#method varible
+var_7 = var_1 % var_2
+print(var_7)
+
+#Division Floor
+var_8 = var_1  // var_2
+print(var_8)
+
+#Power Operation
+var_pow = var_1 ** var_2
+print(var_pow)
+
+
+
+#Square Root
+#import math
+#var = 100
+#math.sqrt(var)
+
+
