118333f6cc8c8ba5bcd0ffdcf9eac5b276904d4d
MissMwendwa <wanjamwesh@gmail.com>
2023-08-16 21:45:26 +0300

    If Statements

---
+++ If Statements/if_statement.py
@@ -0,0 +1,29 @@
+# The simple If statement
+# We use the syntax as indicated below
+#if(criteria):
+#    statements
+#statements
+
+# This is the first example
+var_age = 17
+
+#if section
+if (var_age >= 18):
+    print("Allow into the party")
+print("Go home, you are underage!")
+
+# Its a shopping activity
+var_cloth = ("silk")
+
+#if Section
+if (var_cloth == "cotton"):
+    print(" This is the right Fabric")
+print("The Fabric is Too Soft")
+
+## The camera Scenario
+var = "canon m50 mark ii"
+
+# if section
+if (var == "Sony ZV-1"):
+    print(" Make the Purchase")
+print("Do not Purchase")
